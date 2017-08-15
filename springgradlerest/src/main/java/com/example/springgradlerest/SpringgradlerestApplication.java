da468bce18662fc0aabd9b96cb92ae6c94a4e513
snigdha <sni-ui@users.noreply.github.com>
2017-08-15 16:13:56 -0500

    Add files via upload

---
+++ springgradlerest/src/main/java/com/example/springgradlerest/SpringgradlerestApplication.java
@@ -0,0 +1,12 @@
+package com.example.springgradlerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgradlerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgradlerestApplication.class, args);
+	}
+}
---
+++ springgradlerest/src/test/java/com/example/springgradlerest/SpringgradlerestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springgradlerest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringgradlerestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
